fefd8a5d66b7b0169e5963a99c170a61eb528ea5
Marcel Muslija <marcel.muslija@gmail.com>
2018-04-08 12:22:51 +0200

    Added minor functionalities

--- blackjack_cpp/gameMechanics.cpp
+++ blackjack_cpp/gameMechanics.cpp
@@ -1,17 +1,34 @@
 #include <array>
+#include <conio.h>
+#include <windows.h>
 #include "deck.h"
 #include "player.h"
 #include "io.h"
 
+
+static void drawCard(Player &player, Card **ptrCardPtr)
+{
+    printDraw(player);
+    Sleep(2000);
+    printCard(**ptrCardPtr);
+    Sleep(500);
+    addScore(player, getCardValue(**ptrCardPtr));
+    (*ptrCardPtr)++;
+}
+
+static void initialDraw(Player player, Player dealer, Card **ptrCardPtr)
+{
+    drawCard(dealer, ptrCardPtr);
+    drawCard(player, ptrCardPtr);
+    drawCard(player, ptrCardPtr);
+}
+
 bool playBlackjack(Deck &deck)
 {
     Card *cardPtr = &deck[0];
+    Card **ptrCardPtr = &cardPtr;
     Player player = createPlayer(getPlayerName(), 0);
-    Player dealer = createPlayer("Jack", 0);
+    Player dealer = createPlayer("Dealer", 0);
 
-    addScore(dealer, getCardValue(*cardPtr++));
-    addScore(player, getCardValue(*cardPtr++));
-
-    printPlayerScore(dealer);
-    printPlayerScore(player);
+    initialDraw(player, dealer, ptrCardPtr);
 }
